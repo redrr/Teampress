8d9ee1ee35c8dd0ff8b6c5766906710cb1e2ffec
mredly <mredly@konzorcia.hu>
2020-10-10 13:23:18 +0200

    test

--- application/playmaker_v0.1/src/main/java/hu/playmaker/PlaymakerApplication.java
+++ application/playmaker_v0.1/src/main/java/hu/playmaker/PlaymakerApplication.java
@@ -3,9 +3,10 @@ package hu.playmaker;
 import com.google.api.services.fitness.model.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlaymakerApplication {
+public class PlaymakerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","chromedriver_85.exe");
